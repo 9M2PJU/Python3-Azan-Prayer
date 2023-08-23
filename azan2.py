7bc6ca94de012957cb5cea63b82ae992fb977505
Piju <9m2pju@gmail.com>
2023-08-24 06:35:30 +0800

    Update azan2.py

--- azan2.py
+++ azan2.py
@@ -1,66 +1,63 @@
 import requests
 import subprocess
-from datetime import datetime, timedelta
+from datetime import datetime
 import time
 
 # Global variables to store fetched prayer times and last played times
 prayer_times_cache = []
 last_played_times = {}
 
-def fetch_prayer_times():
-    global prayer_times_cache
-    
-    # Fetch prayer times only if the cache is empty or more than 5 times a day has passed
-    if not prayer_times_cache or (datetime.now() - prayer_times_cache[-1]['fetch_time']).total_seconds() >= 86400 / 5:
-        url = "http://mpt.i906.my/mpt.json?code=wlp-0&filter=1"
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            data = response.json()
-            prayer_times = data['response']['times']
-            fetch_time = datetime.now()
-            prayer_times_cache = [{'time': time_entry, 'fetch_time': fetch_time} for time_entry in prayer_times]
-        except requests.exceptions.RequestException as e:
-            print("An error occurred while fetching prayer times:", e)
+def fetch_prayer_times(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        prayer_times = data['response']['times']
+        return prayer_times
+    except requests.exceptions.RequestException as e:
+        print("An error occurred while fetching prayer times:", e)
+        return []
 
 def play_azan(audio_file):
     try:
-        subprocess.run(['mpg321', audio_file], check=True)
+        subprocess.run(['mpg123', audio_file], check=True)
         last_played_times[audio_file] = datetime.now()  # Record last played time
     except subprocess.CalledProcessError:
         print("Error playing Azan audio:", audio_file)
 
 def main():
-    while True:
-        fetch_prayer_times()
+    fetch_url = "https://mpt.i906.my/mpt.json?code=wlp-0&filter=1"
 
-        if not prayer_times_cache:
-            print("No prayer times data available.")
-            time.sleep(7200)  # Wait for 2 hours
-            continue
+    while True:
+        current_time = datetime.now()
+        
+        # Fetch prayer times now
+        print("Fetching current prayer times...")
+        prayer_times = fetch_prayer_times(fetch_url)
+        if prayer_times:
+            prayer_times_cache = prayer_times
 
         azan_files = {
-            "Fajr": "azan2.mp3",
-            "Dhuhr": "azan.mp3",
-            "Asr": "azan.mp3",
-            "Maghrib": "azan.mp3",
-            "Isha": "azan.mp3"
+            0: "azan2.mp3",  # Fajr
+            1: "azan.mp3",   # Dhuhr, Asr, Maghrib, Isha
         }
 
-        current_time = datetime.now()
-
-        for time_entry in prayer_times_cache:
-            prayer_time = datetime.fromtimestamp(time_entry['time'])
-            prayer_name = datetime.strftime(prayer_time, "%H:%M")
+        for idx, prayer_time in enumerate(prayer_times_cache):
+            if idx == 0:  # Fajr
+                azan_file = azan_files[0]
+            elif idx == 1:  # Sunrise, skip
+                continue
+            else:  # Dhuhr, Asr, Maghrib, Isha
+                azan_file = azan_files[1]
             
-            if prayer_name in azan_files and prayer_name not in last_played_times:
-                time_difference = (prayer_time - current_time).total_seconds()
-                if 0 <= time_difference <= 300:  # Within 5 minutes
-                    print(f"It's time for {prayer_name} prayer!")
-                    play_azan(azan_files[prayer_name])
-        
-        # Wait for 2 minutes before checking again
-        time.sleep(120)
+            prayer_datetime = datetime.fromtimestamp(prayer_time)
+            time_difference = (prayer_datetime - current_time).total_seconds()
+            
+            if 0 <= time_difference <= 300:  # Within 5 minutes
+                print(f"It's time for {idx} prayer!")
+                play_azan(azan_file)
+
+        time.sleep(3600)  # Wait for 1 hour before checking again
 
 if __name__ == "__main__":
     main()
